aa24c2ac29e8894e8ca073a425023ea8c9bd4803
mau5atron <gabrielbetancourt96@outlook.com>
2017-11-10 01:23:41 -0500

    added the ability to create tasks using ajax when the form_with in _form's local:true is changed to remote:true. Changed _form to remote:true when tasks are saved the user is redirected to the index page where task are shown. New private method defined when creeating a new task to clean up code in index action in controller. Added jquery to application.js to have ability to run jquery in task.js. Added gem jquery-rails in gemfile. Added jquery to task.js that checks when turblinks fully loads and prints to console that document is ready. In index.html.erb the new task form is placed by rendering partial in index from _form file

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,8 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+$(document).on('turbolinks:load', function() {
+	// jquery checks to see when turbolinks fully load then a function is executed
+  console.log('document is ready!', new Date()); // this logging is optional
+  $('#task_name').focus();
+});
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,88 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :new_task, only: [:new, :index]
+  #new before action new_task allows the new action to happen on the index page 
+
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    @tasks = Task.all
+    
+    # added the ability to add a task in the index page
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+    # removed @task = Task.new and moved to private method new_task
+    # in order to dry up code and also to not reload the page each time
+    # the user makes a new task
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        # format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        # This prevents the redirect to the tasks#show
+        format.html { redirect_to tasks_url }
+        # instead we redirect into the task list
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.require(:task).permit(:name)
+    end
+
+    # Added new task ajax on homepage
+    def new_task
+      @task = Task.new
+    end
+end
